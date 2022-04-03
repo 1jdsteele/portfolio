6a3bd44a7fa52b74e623cdcb19ee9c67960f379a
jdsteele <jdsteelepersonal@gmail.com>
2022-04-02 19:06:56 -0700

    basic layout complete in grid: header, aside on left for nav, main, and aside on right for photos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        Jake Steele: Full Stack Software Engineer. This is in the header.
       </header>
+      <main className="main">
+        This is where the meat and potatoes of information would go, inside of a main.
+      </main>
+      <aside className="sidebar">
+        <nav>
+          This is where the routes in the sidebar to the left would go to which is in a nav in an aside.
+        </nav>
+      </aside>
+      <aside className="some-pics-right">
+        This is where some pics would go, an aside on the right.
+      </aside>
+      
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// KEEPING THIS TEST FOR NOW TO REPLACE WITH MY OWN TESTS LATER
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
