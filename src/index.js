f39259f9b87c65a66a9988d8f66313624cb2a730
jdsteele <jdsteelepersonal@gmail.com>
2022-04-23 09:57:01 -0700

    index work

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//next line from reactjs.org
+import { createRoot } from 'react-dom/client';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+// pasted from reactjs .org
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<React.StrictMode>
+  <Router>
+    <App />
+  </Router>
+</React.StrictMode>,
+); 
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
