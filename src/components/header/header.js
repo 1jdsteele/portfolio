c88c6ed7653dd61b1919f513b5ff41c251fa4342
jdsteele <jdsteelepersonal@gmail.com>
2022-04-22 17:47:55 -0700

    added face pic to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,13 +4,18 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <div>
-        <h1>
-          Jake Steele:
-        </h1>
-        <h2>
-          Full Stack Software Engineer. 
-        </h2>
+      <div className="headerFlex">
+        <div>
+          <h1>
+            Jake Steele:
+          </h1>
+          <h2>
+            Full Stack Software Engineer. 
+          </h2>
+        </div>
+        <div className="headerImage">
+          <img src="/IMG_7484.jpeg" alt="face photo" />
+        </div>
       </div>
         
     </header>
